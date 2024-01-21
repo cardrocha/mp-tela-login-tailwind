5a0faf82e591fef1685b66aad2a90a93541b178b
cardrocha <cardrocharj@gmail.com>
2024-01-21 19:33:07 -0300

    style: message welcome page

--- src/app/welcome/page.tsx
+++ src/app/welcome/page.tsx
@@ -2,9 +2,10 @@ import Image from "next/image"
 
 const Welcome = () => {
   return (
-    <div className="flex justify-center">
-      <h1 className="text-4xl mt-36">Bem-vindo a minha tela de login - mini-projeto</h1>
-    </div>
+    <>
+      <h1 className="text-4xl mt-36 text-center">Bem-vindo a minha tela de login - mini-projeto</h1>
+      <p className="text-center text-lg mt-4">Optei por fazer meu próprio design, não está idêntico ao figma, apenas segui que era requisito para o projeto.</p>
+    </>
   )
 }
 
